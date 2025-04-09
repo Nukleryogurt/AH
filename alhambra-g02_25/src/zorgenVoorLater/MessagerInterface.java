b990a1958b200328d18cc06efd2ad6e2a741ebfa
Can Cakal <can.cakal@student.hogent.be>
2025-04-09 13:15:46 +0200

    Delete alhambra-g02_25 directory

--- alhambra-g02_25/src/zorgenVoorLater/MessagerInterface.java
+++
@@ -1,5 +0,0 @@
-package zorgenVoorLater;
-/** Om éénvoudige berichten/informatie te tonen als UI. **/
-public interface MessagerInterface {
-	void showMessage(String message);
-}
--- alhambra-g02_25/src/zorgenVoorLater/NieuweSpelerInterface.java
+++
@@ -1,7 +0,0 @@
-package zorgenVoorLater;
-
-/** Interface om UI en domeincontroller te verbinden bij het registreren van een nieuwe gebruiker.**/
-public interface NieuweSpelerInterface {
-	String getGebruikersnaam();
-	int getGeboortejaar();
-}
